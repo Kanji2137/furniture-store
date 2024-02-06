e69fb87dab37111c0cfbf3a0cffe38c373edc20d
Kanji2137 <samuel.zabczynski@gmail.com>
2024-02-06 20:45:11 +0100

    Update responsiveness product page

--- src/Pages/CatalogPage/Product/Product.js
+++ src/Pages/CatalogPage/Product/Product.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function Product({name, price, description, url}) {
   return (
-    <Link to="/product" key={name} className="product" state={{name, price, description, url}}>
-      <div className={`product__content`}>
-          <div className="product__image">
-            <img src={`${url}.jpg`} alt={name} />
-          </div>
-          <div className="product__title">
-            <h1>{name}</h1>
-            <h2>{price} $</h2>
-          </div>
-      </div>
-    </Link>
+      <Link to="/product" key={name} className="product" state={{name, price, description, url}}>
+        <div className="product__image">
+          <img src={`${url}.jpg`} alt={name}/>
+        </div>
+        <div className="product__title">
+          <h1>{name}</h1>
+          <h2>{price} $</h2>
+        </div>
+      </Link>
   );
 }
 
